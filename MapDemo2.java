2d1f4f2a84eb7ee466943dcb928acc0e201598c4
Neha <96060160+hegdeneha02@users.noreply.github.com>
2023-09-16 15:03:14 +0530

    Create MapDemo2
    Date:14/09/2023

---
+++ MapDemo2.java
@@ -0,0 +1,15 @@
+package collection;
+import java.util.*;
+public class MapDemo2 {
+
+	public static void main(String[] args) {
+		Map<String,String>map=new HashMap<String,String>();
+		map.put("First", "Way");
+		map.put("Second", "Gate");
+		map.put("Third", "Wall");
+		map.put("Fourth", "Person");
+		System.out.println(map);
+
+	}
+
+}
