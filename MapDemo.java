5907de6e3d9b7301a45e3fd928324f3a1bb70f34
Neha <96060160+hegdeneha02@users.noreply.github.com>
2023-09-16 15:02:20 +0530

    Create MapDemo
    Date:14/09/2023

---
+++ MapDemo.java
@@ -0,0 +1,12 @@
+package collection;
+import java.util.*;
+public class MapDemo {
+     public static void main(String []args) {
+    	 Map<Integer,String>map=new HashMap<Integer,String>();
+    	 System.out.println(map);
+    	 map.put(96, "Dinesh");
+    	 map.put(80, "Merin");
+    	 map.put(100, "Swathi");
+    	 System.out.println(map); 
+     }
+}
